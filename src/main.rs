a7eb05ef350ceaa78b4016af121251c0746ffa02
serj <stubin87@gmail.com>
2022-05-20 21:14:08 +0200

    get host and port via args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use crate::router::Router;
 use std::net::TcpListener;
-use std::thread;
+use std::{env, process, thread};
 
 // TODO:
 // create router, that would parse paths and route to related handler
@@ -15,9 +15,8 @@ pub mod router;
 fn main() {
     println!("starting url shortener ...");
 
-    // TODO: configurable via app args
-    let host = "127.0.0.1";
-    let port: u16 = 8080;
+    let (host, port) = get_host_and_port();
+
     let address = format!("{}:{}", host, port);
     println!("will be listening on: {}", address);
 
@@ -39,4 +38,35 @@ fn main() {
             }
         }
     }
+}
+
+fn get_host_and_port() -> (String, u16) {
+    let mut host = "127.0.0.1";
+    let mut port: u16 = 8080;
+
+    let args: Vec<String> = env::args().collect();
+    for i in 0..args.len() {
+        if args[i] == "-host" || args[i] == "-h" {
+            if i + 1 == args.len() {
+                eprintln!("invalid arguments [host], args len: {}", args.len());
+                process::exit(1);
+            }
+            host = args[i+1].as_str();
+        }
+        if args[i] == "-port" || args[i] == "-p" {
+            if i + 1 == args.len() {
+                eprintln!("invalid arguments [port], args len: {}", args.len());
+                process::exit(1);
+            }
+            match args[i+1].parse::<u16>() {
+                Ok(n) => { port = n },
+                Err(e) => {
+                    eprintln!("invalid port argument: {e}");
+                    process::exit(1);
+                },
+            }
+        }
+    }
+
+    (String::from(host), port)
 }
