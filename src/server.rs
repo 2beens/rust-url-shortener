9f7ad3cd44a9350e1a3e40eaeeff2da06e5de6a8
serj <stubin87@gmail.com>
2022-05-21 10:15:38 +0200

    thread pool control for requests

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use crate::router::Router;
+use crate::ThreadPool;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
@@ -19,12 +20,13 @@ impl Server {
         let listener = TcpListener::bind(&self.address).unwrap();
         println!("listening for connections ...");
 
-        // TODO: control requests via Thread Pool
+        // control requests via Thread Pool
+        let pool = ThreadPool::new(5);
 
         for stream in listener.incoming() {
             match stream {
                 Ok(stream) => {
-                    thread::spawn(|| {
+                    pool.execute(|| {
                         // TODO: try to use the shared router
                         // self.router.route(stream);
                         Router::new(false, true).with_logs().route(stream);
